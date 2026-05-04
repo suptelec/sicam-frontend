c93f8559d3b1b33a26c23a280f36e29c074b384c
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-05-04 09:29:08 -0500

    envoromentes produccion real

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.sicam.com',
-  identityUrl: 'https://identity.sicam.com',
+  apiUrl: 'https://sicam-backend-20260503-2101.onrender.com',
+  identityUrl: 'https://identity-20260503-2125.onrender.com',
   oidc: {
-    issuer: 'https://identity.sicam.com',
-    redirectUri: 'https://sicam.com/auth/callback',
-    postLogoutRedirectUri: 'https://sicam.com/auth/logout',
+    issuer: 'https://identity-20260503-2125.onrender.com',
+    redirectUri: 'https://TU_FRONTEND_RENDER.onrender.com/auth/callback',
+    postLogoutRedirectUri: 'https://TU_FRONTEND_RENDER.onrender.com/auth/logout',
     clientId: 'scm-spa',
     scope: 'openid profile offline_access scm-api',
     requireHttps: true
